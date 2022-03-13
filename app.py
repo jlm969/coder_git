9abb89b9bf44f7e7a2fe3d3b5d4b0594d4f090ec
Jorge Marquez <jlm969969@gmail.com>
2022-03-13 18:19:13 -0300

    funcion saludar

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-print("Hello GIT---ahora esoty en la rama mensajes realizando estos cambios")
-print("Hola Mundo")
+print("Hello GIT")
+print("Hola Mundo!!!!")
+print("Chauuuuu")
+
+
+
+def saludar(nombre):
+    return f"Hola {nombre} !!!"
+
+print(saludar("Jorge"))
+print(saludar("Liset"))
